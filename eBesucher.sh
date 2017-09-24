2a262bf88be6a63643d3afb977b2edd1a0eedd07
5aimiku <5aimiku@users.noreply.github.com>
2017-09-24 17:26:09 +0800

    Update eBesucher.sh

--- eBesucher.sh
+++ eBesucher.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get -q -y --force-yes install tightvncserver xterm jwm mercurial libasound2-dev libcurl4-openssl-dev libnotify-dev libxt-dev libiw-dev mesa-common-dev autoconf2.13 yasm libidl-dev screen unzip bzip2 nano
 wget http://ftp.mozilla.org/pub/firefox/releases/32.0/linux-i686/en-US/firefox-32.0.tar.bz2
-wget http://atomstar.eu.org/eb/vncxstartup.txt http://atomstar.eu.org/eb/vncserverinit.txt
+wget https://raw.githubusercontent.com/5aimiku/ebesucher-for-debian/master/vncxstartup.txt https://raw.githubusercontent.com/5aimiku/ebesucher-for-debian/master/vncserverinit.txt
 tar xjf firefox-32.0.tar.bz2
 mv firefox/ /usr/local/lib/
 ln -s /usr/local/lib/firefox/firefox /usr/bin/firefox
